c4983d08ce5e9a57b69a963c40ead6beb0b763a3
A.J. Psyk <ajpsyk@gmail.com>
2026-06-07 19:32:04 -0700

    happy number

---
+++ 4. Fast and Slow Pointers/202. Happy Number/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while (true)
+        {   
+            slow = square_then_sum_digits(slow);
+            fast = square_then_sum_digits(square_then_sum_digits(fast));
+            if (fast == 1) return true;
+            if (slow == fast) return false;   
+        }
+    }
+
+    int square_then_sum_digits(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            sum += std::pow(n % 10, 2);
+            n /= 10;
+        }
+        return sum;
+    }
+};
